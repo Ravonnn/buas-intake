5ce61033e192c03070243718934e0a28e30631a5
Timo van Balen <90253260+Ravonnn@users.noreply.github.com>
2023-01-17 17:14:57 +0100

    Initial Commit

---
+++ src/pch.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <cstdio> //printf
+#include <cstdlib>
+#include <cassert>
+#include <cstring>
+#include <fcntl.h>
+#include <io.h>
+#include <corecrt_math.h>
+#include <cstdio>
+#include <iostream>
+#include <SDL.h> 
+
+#include "Engine/surface.h"
+#include "Engine/template.h"
